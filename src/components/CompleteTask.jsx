c82c4a6a58c9a105ba030086b688aa6717300926
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 10:38:33 -0700

    create complete method with console log for each task

--- src/components/CompleteTask.jsx
+++ src/components/CompleteTask.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+function complete() {
+  console.log("complete clicked");
+}
+
 function CompleteTask(props) {
   return (
     <div>
-      <button>Do it</button>
+      <button onClick={complete}>Do it</button>
     </div>
   );
 }
