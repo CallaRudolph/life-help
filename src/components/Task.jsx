dc51f9d259727404b504e4a94a1b1d81bb5244e9
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 09:49:16 -0700

    import prop types into TaskList and Task components

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Task(props) {
   return (
     <div>
-      <h3>Test task</h3>
+      <h3>{props.task}</h3>
+      <h4>Complete {props.frequency}</h4>
     </div>
   );
 }
+
+Task.propTypes = {
+  task: PropTypes.string.isRequired,
+  frequency: PropTypes.string.isRequired
+};
 
 export default Task;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Task from "./Task";
+import PropTypes from "prop-types";
 
 function TaskList(props) {
   return (
-    <Task />
+    <div>
+      {props.taskList.map((task, index) =>
+        <Task task={task.task}
+              frequency={task.frequency}
+            key={index}/>
+      )}
+    </div>
   );
+}
+
+TaskList.propTypes = {
+  taskList: PropTypes.array
 }
 
 export default TaskList;
