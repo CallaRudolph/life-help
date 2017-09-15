742f466320e14d835b48459e7f2aef57e5054f61
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 13:24:54 -0700

    update app title and timeCompleted variable

--- src/components/CompleteTask.jsx
+++ src/components/CompleteTask.jsx
@@ -8,7 +8,7 @@ class CompleteTask extends React.Component {
   constructor(props) {
     super(props);
     this.state = {showComplete: false,
-                  test: null};
+                  completedTime: null};
     this.completeTask = this.completeTask.bind(this);
   }
 
@@ -17,19 +17,24 @@ class CompleteTask extends React.Component {
     this.setState({showComplete: newShowComplete});
     var date = new Moment();
     var dateString = date.format("MMM Do, h:mm a");
-    this.setState({test: dateString});
+    this.setState({completedTime: dateString});
   }
 
   render() {
     console.log(this.props.taskName);
     let formAreaContent = null;
     if (this.state.showComplete === false) {
-      formAreaContent = <Button bsStyle="success" onClick={this.completeTask}>{this.props.taskName}</Button>
+      formAreaContent =
+        <Button
+          bsStyle="success" onClick={this.completeTask}>
+          {this.props.taskName}</Button>
     } else {
       formAreaContent =
       <div>
-        <p><em>Completed: {this.state.test} </em></p>
-      <Button bsStyle="success" onClick={this.completeTask}>{this.props.taskName} again</Button>
+        <p><em>Completed: {this.state.completedTime} </em></p>
+        <Button
+          bsStyle="success" onClick={this.completeTask}>
+          {this.props.taskName} again</Button>
       </div>
     }
     return (
