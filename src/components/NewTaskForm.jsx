c58c64de0f8f6fc2fce0f98105fa13e8c22d54df
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 09:28:43 -0700

    gather Refs data

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -2,24 +2,32 @@ import React from "react";
 
 class NewTaskForm extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.handleNewTask = this.handleNewTask.bind(this);
+  }
+
   handleNewTask(event) {
     event.preventDefault()
+    const { _task, _frequency } = this.refs;
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={handleNewTask}>
+        <form onSubmit={this.handleNewTask}>
           <textarea
             ref="_task"
             type="text"
             id="task"
             placeholder="What do you need to do?"/>
+          <br/>
           <input
             ref="frequency"
             type="text"
             id="frequency"
             placeholder="Frequency"/>
+          <br/><br/>
           <button type="submit">Add task</button>
         </form>
       </div>
