5c4d356d1bb009342e6767f5173ed9dfbbcf12a7
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 09:35:43 -0700

    create pure Task child component with import into TaskList

---
+++ src/components/Task.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Task(props) {
+  return (
+    <div>
+      <h3>Test task</h3>
+    </div>
+  );
+}
+
+export default Task;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Task from "./Task";
 
 function TaskList(props) {
-  return ();
+  return (
+    <Task />
+  );
 }
 
 export default TaskList;
