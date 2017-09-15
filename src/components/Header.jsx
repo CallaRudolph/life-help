24e80d44893dc05c7ecf1619fb253623829bffa0
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 15:30:11 -0700

    enter content and styling for HHeader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 function Header(props) {
+  var header = {
+    backgroundColor: "#7D82B8",
+    textAlign: "center",
+    color: "white",
+    textShadow: "1px 1px #D6F7A3"
+  }
   return (
-    <div>
-      <p>I'm a header</p>
+    <div style={header} className="container">
+      <h1><em>This is a super fancy Task List. Enter your tasks and mark them complete when you do them. Keep up with it!</em></h1>
     </div>
   );
 }
