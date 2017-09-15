fb5bd2585459af4a34e5a4cd65c8577d106d2463
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 14:59:33 -0700

    install css/url/file loader toder to webpack to import background image - FAIL

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import CompleteTask from "./CompleteTask";
+import Background from '../images/background.jpg';
 
 function Task(props) {
+  let img = '..images/background.jpg'
   var flexItem = {
     margin: "5",
     order: "1",
@@ -11,11 +13,14 @@ function Task(props) {
     minWidth: "275",
     maxWidth: "290",
   }
+  var well = {
+    backgroundColor: "#b7e3cc",
+  }
   return (
     <ul className="list-unstyled">
       <div style={flexItem}>
         <li>
-          <div className="well">
+          <div style={well} className="well">
             <h3>{props.task}</h3>
             <h4>Complete {props.frequency}</h4>
             <CompleteTask taskName={props.task}/>
--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,30 @@ module.exports = {
           ]
         }
       },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
     ],
   },
 
