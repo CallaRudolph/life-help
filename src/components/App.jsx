51965ad48d2f207b3c202a430d18f71df547969a
Calla Rudolph <callarudolph@gmail.com>
2017-09-15 16:36:44 -0700

    import google fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,11 @@ import Header from "./Header";
 import Life from "./Life";
 
 function App(props){
+  var app = {
+    fontFamily: "'Poiret One', cursive"
+  }
   return (
-    <div className="container">
+    <div style={app} className="container">
       <Header/>
       <Life/>
     </div>
