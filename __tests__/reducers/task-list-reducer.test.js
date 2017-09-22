0ad778ca69742e5404985f386dfa9c5cbc3277da
Calla Rudolph <callarudolph@gmail.com>
2017-09-22 15:26:49 -0700

    add constants file and testing script

--- __tests__/reducers/task-list-reducer.test.js
+++ __tests__/reducers/task-list-reducer.test.js
@@ -1,9 +1,14 @@
 import taskList from './../../src/reducers/task-list-reducer';
+import c from './../../src/constants';
 
 describe("Task list reducer", () => {
 
   test('should return equivalent state if no action type is recognized', () => {
     expect(taskList([], { type: null })).toEqual([]);
   });
+
+  test('imported value should match action type string', () => {
+  expect(constants.ADD_TASK).toEqual('testing testing 123');
+});
 
 });
