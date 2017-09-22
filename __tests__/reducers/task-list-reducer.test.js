1849b36b1e16b1b9c41780e72d9047e6ac080d62
Calla Rudolph <callarudolph@gmail.com>
2017-09-22 15:32:03 -0700

    write test to add task to list with initial fail

--- __tests__/reducers/task-list-reducer.test.js
+++ __tests__/reducers/task-list-reducer.test.js
@@ -2,13 +2,29 @@ import taskList from './../../src/reducers/task-list-reducer';
 import c from './../../src/constants';
 
 describe("Task list reducer", () => {
+  let action;
+  const taskInfo = {
+    title : "Wash dishes",
+    frequency : "weekly",
+    completed : null,
+    id : 0
+  };
 
   test('should return equivalent state if no action type is recognized', () => {
     expect(taskList([], { type: null })).toEqual([]);
   });
 
-  test('imported value should match action type string', () => {
-  expect(constants.ADD_TASK).toEqual('testing testing 123');
-});
+  test('should add task to list array' () => {
+    const { title, frequency, completed, id } = taskInfo;
+    action = {
+      type: c.ADD_TASK,
+      title: title,
+      frequency: frequency,
+      completed: completed,
+      id: id
+    };
+    const futureState = [ taskInfo ];
+    expect(taskList([], action)).toEqual([ taskInfo ]);
+  });
 
 });
