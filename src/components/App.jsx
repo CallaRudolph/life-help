a35ad5148c67a44718fa7a166c0984e9752bee36
Calla Rudolph <callarudolph@gmail.com>
2017-09-27 11:33:02 -0700

    add background color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,15 @@ import Life from "./Life";
 
 function App(props){
   var app = {
-    fontFamily: "'Poiret One', cursive"
+    fontFamily: "'Poiret One', cursive",
+    backgroundColor: "#fff8f0",
+    backgroundSize: "cover",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    zIndex: 9999,
   }
   return (
     <div style={app} className="container">
